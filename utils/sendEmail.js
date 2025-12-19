d6b863a5e0b068c5f340881ed8ad0f6fd9112a5d
satyco2111-beep <satyco2111@gmail.com>
2025-12-19 18:26:23 +0530

    ddd

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,13 +4,13 @@ export const sendEmail = async (to, subject, text) => {
     const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS, // App password
+            user: "satyco2111@gmail.com",
+            pass: "afnn mapu gfai pyzk", // App password
         },
     });
 
     await transporter.sendMail({
-        from: process.env.EMAIL_USER,
+        from: "satyco2111@gmail.com",
         to,
         subject,
         text,
