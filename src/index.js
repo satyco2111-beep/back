922212938d162100d6a38b80a8086837a3fddfd7
satyco2111-beep <satyco2111@gmail.com>
2025-12-19 15:55:21 +0530

    back url set

--- src/index.js
+++ src/index.js
@@ -21,7 +21,11 @@ connectDB();
 
 app.use(
     cors({
-        origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+        origin: [
+            "http://localhost:3000", 
+            "http://127.0.0.1:3000",
+            "https://back-nwex.onrender.com"
+        ],
         credentials: true,
     })
 );
