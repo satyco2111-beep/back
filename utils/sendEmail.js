ccf71e8c2f475f051d42becb021faa20621ca841
satyco2111-beep <satyco2111@gmail.com>
2025-12-22 14:13:37 +0530

    dddd

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer";
 export const sendEmail = async (to, subject, text) => {
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com', // e.g., 'smtp.mailersend.net'
-        port: 587, // e.g., 587 or 2525
-        secure: false,
+        port: 465, // e.g., 587 or 2525
+        secure: true,
         auth: {
             user: "satyco2111@gmail.com",
             pass: "afnn mapu gfai pyzk", // App password
